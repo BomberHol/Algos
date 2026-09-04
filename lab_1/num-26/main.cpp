6d5d101a07bb9a063577918be67111cd6c0e9a51
Artem Antonov <bouckgoluck973@gmail.com>
2026-09-04 21:04:13 +0300

    Реализовал вывод ответа для английского текста в консоль

--- lab_1/num-26/main.cpp
+++ lab_1/num-26/main.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <locale>
 
 void WriteArr(std::vector<std::string> arr) {
     for (int i = 0; i < arr.size(); i++) {
-        std::cout << arr[i] << std::endl;
+        if (i == 0 || arr[i] == "!" || arr[i] == "?" || arr[i] == ".") {
+            std::cout << arr[i];
+        } else {
+            std::cout << " " << arr[i];
+        }
     }
+    std::cout << std::endl;
 }
 
 std::vector<std::string> SplitWord(std::string word) {
     std::vector<std::string> subwords;
     std::string tempSubword;
+
     for (int i = 0; i < word.length(); i++) {
-        if ((word[i] >= 'A' && word[i] <= 'Z') || (word[i] >= 'A' && word[i] <= 'Z')) {
+        if (std::isalpha(word[i])) {
             tempSubword += word[i];
         } else {
             if (!tempSubword.empty()) {
@@ -30,24 +37,44 @@ std::vector<std::string> SplitWord(std::string word) {
     return subwords;
 }
 
+void UpdateSentence(std::vector<std::string> &lastOffer, const std::vector<std::string> &subwords) {
+    for (int i = 0; i < subwords.size(); i++) {
+        if (!lastOffer.empty() && (lastOffer[lastOffer.size() - 1] == "." || lastOffer[lastOffer.size() - 1] == "!" || lastOffer[lastOffer.size() - 1] == "?")) {
+            lastOffer.clear();
+            lastOffer.push_back(subwords[i]);
+        } else {
+            lastOffer.push_back(subwords[i]);
+        }
+    }
+}
 
+std::vector<std::string> GetLastOfferFromFile(std::ifstream &infile) {
+    std::string word;
+    std::vector<std::string> lastOffer;
+    while (infile >> word) {
+        std::vector<std::string> subwords = SplitWord(word);
+        UpdateSentence(lastOffer, subwords);
+    }
+    return lastOffer;
+}
 
 int main() {
     std::ifstream infile;
     infile.open("text.txt");
 
     if (infile.is_open()) {
-        std::string word;
-        std::vector<std::string> words(0);
+        std::vector<std::string> lastOffer = GetLastOfferFromFile(infile);
 
-        while (infile >> word) {
-            std::vector<std::string> subwords = SplitWord(word);
-            for (int i = 0; i < subwords.size(); i++) {
-                words.push_back(subwords[i]);
+        if (!lastOffer.empty()) {
+            if (lastOffer.back() == "?" && lastOffer.front() == "Where") {
+                WriteArr(lastOffer);
+                lastOffer.front() = "В Караганде";
+                lastOffer.back() = "!";
+                WriteArr(lastOffer);
+            } else {
+                std::cout << "Спасибо за информацию" << std::endl;
             }
         }
-        WriteArr(words);
     }
     infile.close();
 }
-
