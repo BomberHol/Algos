091ef5a4ce7792c82abb6c0f125b6896584499ef
Artem Antonov <bouckgoluck973@gmail.com>
2026-09-05 16:17:08 +0300

    Добавил обработку русского языка

--- lab_1/num-26/main.cpp
+++ lab_1/num-26/main.cpp
@@ -3,6 +3,17 @@
 #include <vector>
 #include <locale>
 
+const int MIN_CODE_RUSSIAN_LETTERS = 0xC0;
+const int MAX_CODE_RUSSIAN_LETTERS = 0xFF;
+const int CODE_UPPERCASE_E = 0xA8;
+const int CODE_LOWERCASE_E = 0xB8;
+
+const std::string STANDART_RESPONCE = "\xD1\xEF\xE0\xF1\xE8\xE1\xEE \xE7\xE0 \xE8\xED\xF4\xEE\xF0\xEC\xE0\xF6\xE8\xFE";
+const std::string TARGET_WORD = "\xC3\xE4\xE5";
+const std::string REPLACEMENT_WORD = "\xC2 \xCA\xE0\xF0\xE0\xE3\xE0\xED\xE4\xE5";
+const std::string TARGET_SYMBOL = "?";
+const std::string REPLACEMENT_SYMBOL = "!";
+
 void WriteArrToCOUT(std::vector<std::string> arr) {
     for (int i = 0; i < arr.size(); i++) {
         if (i == 0 || arr[i] == "!" || arr[i] == "?" || arr[i] == ".") {
@@ -14,12 +25,25 @@ void WriteArrToCOUT(std::vector<std::string> arr) {
     std::cout << std::endl;
 }
 
+bool CheckEnglishLetter(const unsigned char ch) {
+    return (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z');
+}
+
+bool CheckRussianLetter(const unsigned char ch) {
+    return (ch >= MIN_CODE_RUSSIAN_LETTERS && ch <= MAX_CODE_RUSSIAN_LETTERS) || ch == CODE_UPPERCASE_E || ch == CODE_LOWERCASE_E;
+}
+
+bool CheckLetter(const char ch) {
+    const unsigned char convertCh = static_cast<unsigned char>(ch);
+    return CheckEnglishLetter(convertCh)|| CheckRussianLetter(convertCh);
+}
+
 std::vector<std::string> SplitWord(std::string word) {
     std::vector<std::string> subwords;
     std::string tempSubword;
 
     for (int i = 0; i < word.length(); i++) {
-        if (std::isalpha(word[i])) {
+        if (CheckLetter(word[i])) {
             tempSubword += word[i];
         } else {
             if (!tempSubword.empty()) {
@@ -86,12 +110,13 @@ int main() {
     if (infile.is_open()) {
         std::vector<std::string> lastOffer = GetLastOfferFromFile(infile);
         if (!lastOffer.empty()) {
-            if (lastOffer.back() == "?" && lastOffer.front() == "Where") {
-                lastOffer.front() = "В Караганде";
-                lastOffer.back() = "!";
+            if (lastOffer.back() == TARGET_SYMBOL && lastOffer.front() == TARGET_WORD) {
+                const std::string firstWord = REPLACEMENT_WORD;
+                lastOffer.front() = firstWord;
+                lastOffer.back() = REPLACEMENT_SYMBOL;
                 responce = WriteArrToStr(lastOffer);
             } else {
-                responce = "Спасибо за информацию";
+                responce = STANDART_RESPONCE;
             }
         }
     } else {
