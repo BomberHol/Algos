a6eb889c04a74be6014d661ec28775945f8f1d72
Artem Antonov <bouckgoluck973@gmail.com>
2026-09-05 17:16:54 +0300

    Добавить обработку нескольких знаков впороса на концце предложения

--- lab_1/num-26/main.cpp
+++ lab_1/num-26/main.cpp
@@ -8,8 +8,7 @@ const int MAX_CODE_RUSSIAN_LETTERS = 0xFF;
 const int CODE_UPPERCASE_E = 0xA8;
 const int CODE_LOWERCASE_E = 0xB8;
 
-const std::string STANDART_RESPONCE = "\xD1\xEF\xE0\xF1\xE8\xE1\xEE \xE7\xE0 \xE8\xED\xF4\xEE\xF0\xEC\xE0\xF6\xE8\xFE";
-const std::string TARGET_WORD = "\xC3\xE4\xE5";
+const std::string STANDART_RESPONSE = "\xD1\xEF\xE0\xF1\xE8\xE1\xEE \xE7\xE0 \xE8\xED\xF4\xEE\xF0\xEC\xE0\xF6\xE8\xFE";
 const std::string REPLACEMENT_WORD = "\xC2 \xCA\xE0\xF0\xE0\xE3\xE0\xED\xE4\xE5";
 const std::string TARGET_SYMBOL = "?";
 const std::string REPLACEMENT_SYMBOL = "!";
@@ -61,25 +60,29 @@ std::vector<std::string> SplitWord(std::string word) {
     return subwords;
 }
 
-void UpdateSentence(std::vector<std::string> &lastOffer, const std::vector<std::string> &subwords) {
+bool IsPunctuation(std::string itemSentence) {
+    return itemSentence == "." || itemSentence == "!" || itemSentence == "?";
+}
+
+void UpdateLastSentence(std::vector<std::string> &lastSentence, const std::vector<std::string> &subwords) {
     for (int i = 0; i < subwords.size(); i++) {
-        if (!lastOffer.empty() && (lastOffer[lastOffer.size() - 1] == "." || lastOffer[lastOffer.size() - 1] == "!" || lastOffer[lastOffer.size() - 1] == "?")) {
-            lastOffer.clear();
-            lastOffer.push_back(subwords[i]);
+        if (lastSentence.size() > 1 && IsPunctuation(lastSentence[lastSentence.size() - 1]) && !IsPunctuation(subwords[i])) {
+            lastSentence.clear();
+            lastSentence.push_back(subwords[i]);
         } else {
-            lastOffer.push_back(subwords[i]);
+            lastSentence.push_back(subwords[i]);
         }
     }
 }
 
-std::vector<std::string> GetLastOfferFromFile(std::ifstream &infile) {
+std::vector<std::string> GetLastSentenceFromFile(std::ifstream &infile) {
     std::string word;
-    std::vector<std::string> lastOffer;
+    std::vector<std::string> lastSentence;
     while (infile >> word) {
         std::vector<std::string> subwords = SplitWord(word);
-        UpdateSentence(lastOffer, subwords);
+        UpdateLastSentence(lastSentence, subwords);
     }
-    return lastOffer;
+    return lastSentence;
 }
 
 std::string WriteArrToStr(std::vector<std::string> arr) {
@@ -94,42 +97,78 @@ std::string WriteArrToStr(std::vector<std::string> arr) {
     return str;
 }
 
+bool CheckTargetWord(const std::string word) {
+    if (word.length() < 3 || word.length() > 3) {
+        return false;
+    }
+
+    unsigned char firstLetter = static_cast<unsigned char>(word[0]);
+    if (firstLetter == 0xC3 || firstLetter == 0xE3) {
+        return true;
+    }
+
+    unsigned char secondLetter = static_cast<unsigned char>(word[1]);
+    if (secondLetter == 0xC4 || secondLetter == 0xE4) {
+        return true;
+    }
+
+    unsigned char thirdLetter = static_cast<unsigned char>(word[2]);
+    if (thirdLetter == 0xC5 || thirdLetter == 0xE5) {
+        return true;
+    }
+
+    return false;
+}
+
+void ReplaceTargetSymbols(std::vector<std::string> &lastSentence) {
+    for (int i = 0; i < lastSentence.size(); i++) {
+        if (IsPunctuation(lastSentence[i])) {
+            lastSentence[i] = REPLACEMENT_SYMBOL;
+        }
+    }
+}
+
+std::string GenereteResponseFromFile(std::ifstream &infile) {
+    std::string response;
+    std::vector<std::string> lastSentence = GetLastSentenceFromFile(infile);
+    if (!lastSentence.empty()) {
+        if (CheckTargetWord(lastSentence.front()) && lastSentence.back() == TARGET_SYMBOL) {
+            lastSentence.front() = REPLACEMENT_WORD;
+            ReplaceTargetSymbols(lastSentence);
+            response = WriteArrToStr(lastSentence);
+        } else {
+            response = STANDART_RESPONSE;
+        }
+    }
+    return response;
+}
+
 int main() {
     std::string nameFileInput;
     std::cout << "Введите названия INPUT файла: ";
     std::cin >> nameFileInput;
-
     std::string nameFileOutput;
     std::cout << "Введите название OUTPUT файла: ";
     std::cin >> nameFileOutput;
 
-    std::string responce;
-
+    std::string response;
     std::ifstream infile;
     infile.open(nameFileInput);
     if (infile.is_open()) {
-        std::vector<std::string> lastOffer = GetLastOfferFromFile(infile);
-        if (!lastOffer.empty()) {
-            if (lastOffer.back() == TARGET_SYMBOL && lastOffer.front() == TARGET_WORD) {
-                const std::string firstWord = REPLACEMENT_WORD;
-                lastOffer.front() = firstWord;
-                lastOffer.back() = REPLACEMENT_SYMBOL;
-                responce = WriteArrToStr(lastOffer);
-            } else {
-                responce = STANDART_RESPONCE;
-            }
-        }
+        response = GenereteResponseFromFile(infile);
+        infile.close();
     } else {
         std::cerr << "Ошибка чтения INPUT файла!!!" << std::endl;
+        return 1;
     }
-    infile.close();
 
     std::ofstream outfile;
     outfile.open(nameFileOutput);
     if (outfile.is_open()) {
-        outfile << responce << std::endl;
+        outfile << response << std::endl;
+        outfile.close();
     } else {
         std::cerr << "Ошибка записи OUTPUT файла!!!" << std::endl;
+        return 1;
     }
-    outfile.close();
 }
