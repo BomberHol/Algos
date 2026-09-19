b456fcfab6aa2d0d4cd185abb682724f50585212
Artem Antonov <bouckgoluck973@gmail.com>
2026-09-19 19:58:19 +0300

    Реализовал двухсторонию очередь

--- lab_2/num_25/main.cpp
+++ lab_2/num_25/main.cpp
@@ -1,61 +1,126 @@
 #include <iostream>
 
-
+const std::string NULL_STR = "";
 
 class Node {
 public:
     std::string word;
     Node* next;
-    Node(std::string inword) {
-        word = inword;
+    Node* prev;
+    Node(const std::string& inWord) {
+        word = inWord;
+        prev = nullptr;
         next = nullptr;
     }
 };
 
-class Stack {
+class Deque {
 private:
-    Node* first;
-    // Node* end;
+    Node* beg;
+    Node* end;
 
-    void InsertNodeInEnd(Node*& node, std::string inword) {
-        if (node == nullptr) {
-            node = new Node(inword);
-        } else {
-            InsertNodeInEnd(node->next, inword);
-        }
+    void RemoveSingleNode() {
+        delete beg;
+        beg = nullptr;
+        end = nullptr;
+    }
+
+    void RemoveTail() {
+        Node* prev = end->prev;
+        delete end;
+        end = prev;
+        end->next = nullptr;
+    }
+
+    void RemoveHead() {
+        Node* next = beg->next;
+        delete beg;
+        beg = next;
+        beg->prev = nullptr;
     }
 
 public:
-
-
-    Stack() {
-
+    Deque() {
+        beg = nullptr;
+        end = nullptr;
     }
 
-    void add(std::string inword) {
-        if (first == nullptr) {
-            first = new Node(inword);
+    ~Deque() {
+        // clear();
+    }
+
+    void push_front(const std::string& inWord) {
+        if (beg == nullptr) {
+            beg = new Node(inWord);
+            end = beg;
         } else {
-            InsertNodeInEnd(first, inword);
+            Node* newNode = new Node(inWord);
+            newNode->next = beg;
+            beg->prev = newNode;
+            beg = newNode;
         }
+    }
+
+    void push_back(const std::string& inWord) {
+        if (end == nullptr) {
+            end = new Node(inWord);
+            beg = end;
+        } else {
+            Node* newNode = new Node(inWord);
+            newNode->prev = end;
+            end->next = newNode;
+            end = newNode;
+        }
+    }
+
+    void pop_front() {
+        if (beg == nullptr) return;
+        if (beg == end) {
+            RemoveSingleNode();
+            return;
+        }
+        RemoveHead();
+    }
+
+    void pop_back() {
+        if (beg == nullptr) return;
+        if (beg == end) {
+            RemoveSingleNode();
+            return;
+        }
+        RemoveTail();
+    }
+
+    std::string front() {
+        if (beg != nullptr) {
+            return beg->word;
+        }
+        return NULL_STR;
+    }
+
+    std::string back() {
+        if (end != nullptr) {
+            return end->word;
+        }
+        return NULL_STR;
     }
 
     int len() {
         int count = 0;
-        Node*& node1 = first;
-        Node* node2 = first;
-        // while (stack != nullptr) {
-        //     count++;
-        //     stack = stack->next;
-        // }
+        Node* node = beg;
+        while (node != nullptr) {
+            count++;
+            node = node->next;
+        }
         return count;
     }
 };
 
 int main() {
-    Stack* stack = new Stack();
-    stack->add("Hello");
-    stack->add("Goodbye");
+    Deque* deque = new Deque();
+    deque->push_front("0");  // 0
+    deque->push_front("+1");  // +1 0
+    deque->push_back("-1");  // +1 0 -1
 
-    // std::cout << stack->word << " " << ;
+    deque->pop_back();
 }
