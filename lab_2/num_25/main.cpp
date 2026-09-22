00d945596b32727c0baf425146de26f3e04eecff
Artem Antonov <bouckgoluck973@gmail.com>
2026-09-22 10:54:31 +0300

    Начал занова реализовывать стек

--- lab_2/num_25/main.cpp
+++ lab_2/num_25/main.cpp
@@ -4,365 +4,132 @@
 
 const std::string NULL_STR = "";
 
-class Node {
-public:
-    std::string word;
-    Node* next;
-    Node* prev;
-    Node(const std::string& inWord) {
-        word = inWord;
-        prev = nullptr;
-        next = nullptr;
-    }
-};
-
-class Deque {
+class Data {
 private:
-    Node* beg;
-    Node* end;
-
-    void RemoveSingleNode() {
-        delete beg;
-        beg = nullptr;
-        end = nullptr;
-    }
-
-    void RemoveTail() {
-        Node* prev = end->prev;
-        delete end;
-        end = prev;
-        end->next = nullptr;
-    }
-
-    void RemoveHead() {
-        Node* next = beg->next;
-        delete beg;
-        beg = next;
-        beg->prev = nullptr;
-    }
-
+    std::string word;
+    int x;
+    int y;
 public:
-    Deque() {
-        beg = nullptr;
-        end = nullptr;
+    Data(const std::string& inWord, const int inX, const int inY) {
+        word = inWord;
+        x = inX;
+        y = inY;
     }
 
-    Deque(const Deque& inDeque) {
-        beg = nullptr;
-        end = nullptr;
-        Node* node = inDeque.beg;
-        while (node != nullptr) {
-            this->push_back(node->word);
-            node = node->next;
-        }
+    std::string getWord() {
+        return word;
     }
 
-    ~Deque() {
-        Node* node = beg;
-        while (node != nullptr) {
-            Node* next = node->next;
-            delete node;
-            node = next;
-        }
+    int getX() {
+        return x;
     }
 
-    void push_front(const std::string& inWord) {
-        if (beg == nullptr) {
-            beg = new Node(inWord);
-            end = beg;
-        } else {
-            Node* newNode = new Node(inWord);
-            newNode->next = beg;
-            beg->prev = newNode;
-            beg = newNode;
-        }
-    }
-
-    void push_back(const std::string& inWord) {
-        if (end == nullptr) {
-            end = new Node(inWord);
-            beg = end;
-        } else {
-            Node* newNode = new Node(inWord);
-            newNode->prev = end;
-            end->next = newNode;
-            end = newNode;
-        }
-    }
-
-    void pop_front() {
-        if (beg == nullptr) return;
-        if (beg == end) {
-            RemoveSingleNode();
-            return;
-        }
-        RemoveHead();
-    }
-
-    void pop_back() {
-        if (beg == nullptr) return;
-        if (beg == end) {
-            RemoveSingleNode();
-            return;
-        }
-        RemoveTail();
-    }
-
-    std::string front() {
-        if (beg != nullptr) {
-            return beg->word;
-        }
-        return NULL_STR;
-    }
-
-    std::string back() {
-        if (end != nullptr) {
-            return end->word;
-        }
-        return NULL_STR;
-    }
-
-    int len() {  // dev
-        int count = 0;
-        Node* node = beg;
-        while (node != nullptr) {
-            count++;
-            node = node->next;
-        }
-        return count;
-    }
-
-    void write() {
-        Node* node = beg;
-        while (node != nullptr) {
-            std::cout << node->word << std::endl;
-            node = node->next;
-        }
+    int getY() {
+        return y;
     }
 };
 
-bool IsLetter(const char& ch) {
-    return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z');
-}
-
-char GetUppercase(const char& ch) {
-    switch (ch) {
-        case 'a':
-            return 'A';
-        case 'b':
-            return 'B';
-        case 'c':
-            return 'C';
-        case 'd':
-            return 'D';
-        case 'e':
-            return 'E';
-        case 'f':
-            return 'F';
-        case 'g':
-            return 'G';
-        case 'h':
-            return 'H';
-        case 'i':
-            return 'I';
-        case 'j':
-            return 'J';
-        case 'k':
-            return 'K';
-        case 'l':
-            return 'L';
-        case 'm':
-            return 'M';
-        case 'n':
-            return 'N';
-        case 'o':
-            return 'O';
-        case 'p':
-            return 'P';
-        case 'q':
-            return 'Q';
-        case 'r':
-            return 'R';
-        case 's':
-            return 'S';
-        case 't':
-            return 'T';
-        case 'u':
-            return 'U';
-        case 'v':
-            return 'V';
-        case 'w':
-            return 'W';
-        case 'x':
-            return 'X';
-        case 'y':
-            return 'Y';
-        case 'z':
-            return 'Z';
+class Node {
+private:
+    Data* data;
+    Node* prev;
+public:
+    Node(const std::string& inWord, const int inX, const int inY) {
+        data = new Data(inWord, inX, inY);
+        prev = nullptr;
     }
-    return ch;
-}
 
-std::string GetUppercaseStr(const std::string& inWord) {
-    std::string word;
-    for (int i = 0; i < inWord.length(); i++) {
-        word += GetUppercase(inWord[i]);
+    ~Node() {
+        delete data;
     }
-    return word;
-}
 
-void FillDeque(std::ifstream& infile, Deque*& deque) {
-    char ch;
-    std::string word;
-    while (infile.get(ch)) {
-        if (ch == 'N') {
-            ch = 'N';
-        }
+    void setPoint(Node*& inPtr) {
+        prev = inPtr;
+    }
 
-        if (ch == '/') {
-            infile.get(ch);
-            if (ch == '/') {
-                infile.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                continue;
-            }
-        }
+    Node* getPrev() {
+        return prev;
+    }
 
-        if (ch == '\047') {
-            infile.ignore(std::numeric_limits<std::streamsize>::max(), '\047');
-            continue;
-        }
+    Data* getData() {
+        return data;
+    }
+};
 
-        if (ch == '{') {
-            infile.ignore(std::numeric_limits<std::streamsize>::max(), '}');
-            continue;
-        }
+class Stack {
+private:
+    int len;
+    Node* top;
 
-        if (!IsLetter(ch) && !word.empty()) {
-            word = GetUppercaseStr(word);
-            if (word == "BEGIN" || word == "END" || word == "RECORD" || word == "CASE" || word == "REPEAT" || word == "UNTIL") {
-                deque->push_back(word);
-            }
-            word = "";
-        } else if (IsLetter(ch)) {
-            word += ch;
+
+public:
+    Stack() {
+        top = nullptr;
+        len = 0;
+    }
+
+    ~Stack() {
+        while (top != nullptr) {
+            Node* prev = top->getPrev();
+            delete top;
+            top = prev;
         }
     }
 
-    if (!word.empty()) { // нужно вынести
-        word = GetUppercaseStr(word);
-        if (word == "BEGIN" || word == "END" || word == "RECORD" || word == "CASE" || word == "REPEAT" || word == "UNTIL") {
-            deque->push_back(word);
-        }
-        word = "";
-    }
-}
-
-void Flash(Deque*& tempDeque , Deque*& deque) {
-    while (tempDeque->len() > 0) {
-        std::string tempWord = tempDeque->back();
-        tempDeque->pop_back();
-        deque->push_front(tempWord);
-    }
-}
-
-std::string GetOperand(Deque*& deque) {
-    std::string oper = deque->front();
-    deque->pop_front();
-    return oper;
-}
-
-std::string CheckRecord(Deque*& deque, std::string oper, const int depth) {
-    std::string err;
-    if (oper == "RECORD") {
-        if (depth == 0) {
-            oper = GetOperand(deque);
-            if (oper != "END") err = "error ending RECORD";
+    void push(const std::string& inWord, const int inX, const int inY) {
+        if (top == nullptr) {
+            top = new Node(inWord, inX, inY);
+            len++;
         } else {
-            err = "error nesting RECORD";
-        }
-    }
-    return err;
-}
-
-std::string CheckCase(Deque*& deque, std::string oper, const int depth) {
-    std::string err;
-    if (oper == "CASE") {
-        if (depth >= 1) {
-            oper = GetOperand(deque);
-            if (oper != "END") err = "error ending CASE";
-        } else {
-            err = "error nesting CASE";
-        }
-    }
-    return err;
-}
-
-std::string CheckRepeatUntil(Deque*& deque, std::string oper, const int depth) {
-    std::string err;
-    if (oper == "REPEAT" || oper == "UNTIL") {
-        if (depth < 1) {
-            err = "error nesting " + oper;
-        }
-    }
-    return err;
-}
-
-std::string ValidateSyntax(const Deque* inDeque) {
-    Deque* deque = new Deque(*inDeque);
-    int depth = 0;
-    std::string err;
-    while (deque->len() > 0 && err.empty() && depth >= 0) {
-        std::string oper = GetOperand(deque);
-        err = CheckRecord(deque, oper, depth);
-        if (err.empty()) err = CheckCase(deque, oper, depth);
-        if (err.empty()) err = CheckRepeatUntil(deque, oper, depth);
-        if (oper == "BEGIN") depth++;
-        if (oper == "END") depth--;
-    }
-    return err;
-}
-
-std::string HasClosedPairs(Deque* deque) {
-    std::string err;
-    Deque* tempDeque = new Deque();
-
-    while (deque->len() > 0) {
-        std::string oper = GetOperand(deque);
-        if (oper == "BEGIN" || oper == "REPEAT" || oper == "CASE" || oper == "RECORD") {
-            tempDeque->push_back(oper);
-        }
-        if (oper == "END" || oper == "UNTIL") {
-            tempDeque->pop_back();
+            Node* newNode = new Node(inWord, inX, inY);
+            newNode->setPoint(top);
+            top = newNode;
+            len++;
         }
     }
 
-    if (tempDeque->len() > 0) {
-        err = "error vlos";
+    void pop() {
+        if (top != nullptr) {
+            Node* prevNode = top->getPrev();
+            delete top;
+            top = prevNode;
+            len--;
+        }
     }
 
-    return err;
-}
+    Data* getTop() {
+        if (top != nullptr) {
+            return top->getData();
+        }
+        return nullptr;
+    }
 
-std::string Analize(Deque* deque) {
-    std::string err = ValidateSyntax(deque);
-    if (err.empty()) err = HasClosedPairs(deque);
-    return err;
-}
+    int getLen() {
+        return len;
+    }
+};
 
 int main() {
-    Deque* deque = new Deque();
-    std::ifstream infile;
-    infile.open("/home/bomber-hol/Projects/Algos/lab_2/num_25/input.txt");
-    if (infile.is_open()) {
-        FillDeque(infile, deque);
-        std::string err = Analize(deque);
-        if (err != "") {
-            std::cerr << err << std::endl;
-            return 1;
-        } else {
-            std::cout << "It`s OK!" << std::endl;
-        }
-    } else {
-        std::cerr << "Error opening file" << std::endl;
-    }
+    // Deque* deque = new Deque();
+    // std::ifstream infile;
+    // infile.open("/home/bomber-hol/Projects/Algos/lab_2/num_25/input.txt");
+    // if (infile.is_open()) {
+    //     FillDeque(infile, deque);
+    //     std::string err = Analize(deque);
+    //     if (err != "") {
+    //         std::cerr << err << std::endl;
+    //         return 1;
+    //     } else {
+    //         std::cout << "It`s OK!" << std::endl;
+    //     }
+    // } else {
+    //     std::cerr << "Error opening file" << std::endl;
+    // }
+
+    Stack* stack = new Stack();
+    stack->push("1", 1, 1);
+    stack->push("2", 2, 2);
+    stack->pop();
+    Data* data = stack->getTop();
+    std::cout << data->getWord() << " " << data->getX() << " " << data->getY() << std::endl;
 }
